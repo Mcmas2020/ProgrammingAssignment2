9b7cbc5a7348201da8e24b62770c81d49f828e57
Mcmas2020 <casss@mcmaster.ca>
2020-05-20 18:03:48 -0400

    getting ready to push

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache potentially computational heavy matrixes by caching the inverse of the matrix. 
 
-## Write a short comment describing this function
+## Below creates a list of the special matrix. First it set and gets the matrix then sets and gets the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                i <- NULL ## i to be used in the later code
+                set <- function(y) {
+                        x <<- y    ## assign y to x in the parent environment. Sets the matrix
+                        i <<- NULL  
+                }
+                get <- function() x ## gets the matrix
+                setinverse <- function(inverse) i <<- inverse ##set i as the inverse
+                getinverse <- function() i ## get the inverse
+                list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) ## assign functions in a list in the parent environment, naming each element
 }
 
 
-## Write a short comment describing this function
+## Below returns the inverse of the orginal special matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse() ## gets inverse of input
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i) ##checks the whether result is null and returns valid cached inverse to parent environment
+                }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i ## if !is.null is false, the vector will calculate the inverse
 }
